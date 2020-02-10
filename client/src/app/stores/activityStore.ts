f0432ab96b7b0cc764b8f0f7dbc60260280a405d
Balintataw J <balintataw2@gmail.com>
2020-02-10 09:00:09 -0800

    Added sorting and grouping of activites by day date

--- client/src/app/stores/activityStore.ts
+++ client/src/app/stores/activityStore.ts
@@ -15,10 +15,25 @@ class ActivityStore {
   @observable submitting = false;
 
   @computed get activitiesByDate() {
-    return Array.from(this.activityRegistry.values())
+    return this.groupActivitiesByDate(
+      Array.from(this.activityRegistry.values())
+    );
+  }
+
+  groupActivitiesByDate = (activities: IActivity[]) => {
+    const sortedActivities = activities
       .slice()
       .sort((a, b) => Date.parse(a.date) - Date.parse(b.date));
-  }
+    return Object.entries(
+      sortedActivities.reduce((activities, activity) => {
+        const date = activity.date.split("T")[0];
+        activities[date] = activities[date]
+          ? [...activities[date], activity]
+          : [activity];
+        return activities;
+      }, {} as { [key: string]: IActivity[] })
+    );
+  };
 
   @action loadActivities = async () => {
     this.loading = true;
