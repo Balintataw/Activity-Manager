31e496ee824812f1ce5d1522fd14d587e1bb5807
Balintataw J <balintataw2@gmail.com>
2020-02-10 20:16:12 -0800

    Added Exception handling middleware to Application command classes

--- API/Startup.cs
+++ API/Startup.cs
@@ -7,6 +7,8 @@ using Microsoft.Extensions.Hosting;
 using Persistence;
 using MediatR;
 using Application.Activities;
+using FluentValidation.AspNetCore;
+using API.Middleware;
 
 namespace API
 {
@@ -22,7 +24,10 @@ namespace API
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
-      services.AddControllers();
+      services.AddControllers().AddFluentValidation(cfg =>
+      {
+        cfg.RegisterValidatorsFromAssemblyContaining<Create>();
+      });
       services.AddCors(opts =>
       {
         opts.AddPolicy("CorsPolicy", policy =>
@@ -40,9 +45,10 @@ namespace API
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
+      app.UseMiddleware<ErrorHandlingMiddleware>();
       if (env.IsDevelopment())
       {
-        app.UseDeveloperExceptionPage();
+        // app.UseDeveloperExceptionPage();
       }
 
       // app.UseHttpsRedirection();
